9b37b0d2fbec24e3643b571cd8bb7a20558568fd
Zach Soles <zac.soles@gmail.com>
2019-06-10 09:08:14 -0400

    fixed bugs

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { RootState } from './state';
 import { todoReducer } from './todos';
+import { listReducer } from './lists';
 
 export { RootState };
 
 // NOTE: current type definition of Reducer in 'redux-actions' module
 // doesn't go well with redux@4
 export const rootReducer = combineReducers<RootState>({
-  todos: todoReducer as any
+  todos: todoReducer as any,
+  lists: listReducer as any
 });
