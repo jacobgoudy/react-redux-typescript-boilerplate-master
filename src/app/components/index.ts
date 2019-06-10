75a813a86b0fe41c43ce8f649080e322ad2d9b9c
Zach Soles <zac.soles@gmail.com>
2019-06-10 09:58:07 -0400

    Added exports for new list header + footer

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,3 +3,5 @@ export { Header } from './Header';
 export { TodoList } from './TodoList';
 export { TodoItem } from './TodoItem';
 export { TodoTextInput } from './TodoTextInput';
+export { ListFooter } from './ListFooter';
+export { ListHeader } from './ListHeader';
